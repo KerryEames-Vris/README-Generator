1536b84e85591581422a286650e1616ae5a0c986
Kerry Eames <Kerryeames@live.com>
2022-01-13 23:15:03 -0700

    added empty response errors, added template, changed code

--- index.js
+++ index.js
@@ -1,60 +1,212 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-const questions = [
-  {
-    type: "input",
-    name: "title",
-    message: "What is your project's name?",
-  },
-  {
-    type: "input",
-    name: "motivation",
-    message: "Why did you make this project?",
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Give a description of your project",
-  },
-  {
-    type: "input",
-    name: "code styles",
-    message: "What code styles does your project use?",
-  },
-  {
-    type: "input",
-    name: "tech and frameworks",
-    message:
-      "What kind of technology or frameworks does your project incorporate?",
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "what are the steps for installing your project?",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "what are the applications for your projects usage?",
-  },
-  {
-    type: "input",
-    name: "credits",
-    message: "Who is credited on the work?",
-  },
-  {
-    type: "input",
-    name: "how to use",
-    message: "Give a description of how to use your product",
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "Please select which license you would like to list.",
-    choices: ["APACHE 2.O", "BSD 3", "GVL-GPL 3.0", "MIT", "None"],
-  },
-];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is your project's name?",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "I need a value to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "motivation",
+      message: "Why did you make this project?",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "I need a value to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Give a description of your project",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "I need a value to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "codestyle",
+      message: "What code styles does your project use?",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "I need a value to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "frameworks",
+      message:
+        "What kind of technology or frameworks does your project incorporate?",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "I need a value to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "what are the steps for installing your project?",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "I need a value to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "what are the applications for your projects usage?",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "I need a value to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "credits",
+      message: "Who is credited on the work?",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "I need a value to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "howto",
+      message: "Give a description of how to use your product",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "I need a value to continue";
+        }
+      },
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Please select which license you would like to list.",
+      choices: ["APACHE 2.O", "BSD 3", "GVL-GPL 3.0", "MIT", "None"],
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your GitHub username?",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "I need a value to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email?",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "I need a value to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "linkedin",
+      message: "What is your LinkedIn?",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "I need a value to continue";
+        }
+      },
+    },
+  ])
+  .then(
+    ({
+      title,
+      motivation,
+      description,
+      codestyle,
+      frameworks,
+      installation,
+      usage,
+      credits,
+      howto,
+      license,
+      github,
+      email,
+      linkedin,
+    }) => {
+      const template = `# ${title}
+
+  * [Motivation](#motivation)
+  * [Description](#description)
+  * [Codestyle](#codestyle)
+  * [Frameworks](#frameworks)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [Howto](#howto)
+  * [License](#license)
+  ## Motivation
+  ${motivation}
+  ## Description
+  ${description}
+  # Code Styles
+  ${codestyle}
+  # Frameworks and Tech
+  ${frameworks}
+  # Installation
+  ${installation}
+  ## Usage
+  ${usage}
+  # Credits
+  ${credits}
+  ## How to Use
+  ${howto}
+  ## License
+  ${license}
+
+  # Contact
+  * GitHub :${github}
+  * E-mail :${email}
+  * Linkedin :${linkedin}`;
+    }
+  );
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
